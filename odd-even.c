eb949b56f51167a03e68e8edbcec3a5b5b4d59fa
Ashutosh Dubey <mail.devashutosh@gmail.com>
2020-10-21 16:37:22 +0530

    Add files via upload

---
+++ odd-even.c
@@ -0,0 +1,22 @@
+// write a program to check if the given number provided by the user is even or odd,
+// if the number is even add any off value to it else then generate the multiplication table of the odd value
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a % 2 == 0)
+    {
+        printf("%d", a + 3);
+    }
+    else
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            printf("%d x %d = %d\n", a, i, a * i);
+        }
+    }
+    return 0;
+}
