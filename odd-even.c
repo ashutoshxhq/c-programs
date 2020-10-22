ff705715b9c6533169da10e164c0ccda82ad771d
Ashutosh Dubey <mail.devashutosh@gmail.com>
2020-10-22 07:45:02 +0530

    Update odd-even.c

--- odd-even.c
+++ odd-even.c
@@ -1,5 +1,5 @@
 // write a program to check if the given number provided by the user is even or odd,
-// if the number is even add any off value to it else then generate the multiplication table of the odd value
+// if the number is even add any odd value to it else generate the multiplication table of the odd value
 
 #include <stdio.h>
 
@@ -8,7 +8,8 @@ int main()
     int a;
     scanf("%d", &a);
     if (a % 2 == 0)
-    {
+    {   
+        a += 3;
         printf("%d", a + 3);
     }
     else
